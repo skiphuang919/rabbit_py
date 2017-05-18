6a7c912f526b7b85f3018cf72e8c1a7112961cfa
ubuntu16.04 <huangkai919@163.com>
2017-05-18 11:34:19 +0800

    modify oo model

--- receive.py
+++ receive.py
@@ -1,34 +1,42 @@
 #!/usr/bin/env python
 import pika
 
-# create a localhost connection obj that is pass to connection adapter
-con_obj = pika.ConnectionParameters(host='localhost')
 
-# create a instance of connection object
-connection = pika.BlockingConnection(con_obj)
+class Consumer(object):
+    def __init__(self):
+        # create a localhost connection obj that is pass to connection adapter
+        self.con_obj = pika.ConnectionParameters(host='localhost')
 
-# create a new channel
-# that is to say create a new connection
-channel = connection.channel()
+        # create a instance of connection object
+        self.connection = pika.BlockingConnection(self.con_obj)
 
-# declare a 'hello' queue, make sure it exists
-# only one 'hello' queue will be created even you declare many times
-channel.queue_declare(queue='hello')
+        # create a new channel
+        # that is to say create a new connection
+        self.channel = self.connection.channel()
 
+    @staticmethod
+    def c1_callback(ch, method, properties, body):
+        # declare callback function
+        # it is called when consume msg
+        print(" [x] Received %r" % body)
 
-# declare callback function
-# it is called when receive msg
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+    def c1(self):
+        # declare a 'hello' queue, make sure it exists
+        # only one 'hello' queue will be created even you declare many times
+        self.channel.queue_declare(queue='hello')
 
-# tell Rabbit that this particular callback function should receive messages
-# from our 'hello' queue
-# `no_ack` means tell the broker not to expect a response
-channel.basic_consume(callback,
-                      queue='hello',
-                      no_ack=True)
-print(' [*] Waiting for messages. To exit press CTRL+C')
+        # tell Rabbit that this particular callback function should receive messages
+        # from our 'hello' queue
+        # `no_ack` means tell the broker not to expect a response
+        self.channel.basic_consume(self.c1_callback,
+                                   queue='hello',
+                                   no_ack=True)
+        print(' [*] Waiting for messages. To exit press CTRL+C')
 
-# enter a never-ending loop that waits for data
-# runs callbacks whenever necessary
-channel.start_consuming()
+        # enter a never-ending loop that waits for data
+        # runs callbacks whenever necessary
+        self.channel.start_consuming()
+
+if __name__ == '__main__':
+    c = Consumer()
+    c.c1()
--- send.py
+++ send.py
@@ -1,28 +1,40 @@
 #!/usr/bin/env python
 import pika
 
-# create a localhost connection obj that is pass to connection adapter
-con_obj = pika.ConnectionParameters(host='localhost')
 
-# create a instance of connection object
-connection = pika.BlockingConnection(con_obj)
+class Producer(object):
+    """
+    obj to produce msg
+    """
+    def __init__(self):
+        # create a localhost connection obj that is pass to connection adapter
+        self.con_obj = pika.ConnectionParameters(host='localhost')
 
-# create a new channel
-# that is to say create a new connection
-channel = connection.channel()
+        # create a instance of connection object
+        self.connection = pika.BlockingConnection(self.con_obj)
 
-# declare a 'hello' queue, create if needed
-channel.queue_declare(queue='hello')
+        # create a new channel
+        # that is to say create a new connection
+        self.channel = self.connection.channel()
 
-# publish msg to the channel with the default exchange
-# cause msg can never be sent directly to queue it always need go through an exchange
-# default exchange is a direct exchange with no name (empty str)
-# within the default exchange situation the routing_key is specified by the queue name
-# in order to tell the msg which queue it should go
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+    def p1(self):
+        # declare a 'hello' queue, create if needed
+        self.channel.queue_declare(queue='hello')
 
-# disconnect from rabbit
-connection.close()
+        # publish msg to the channel with the default exchange
+        # cause msg can never be sent directly to queue it always need go through an exchange
+        # default exchange is a direct exchange with no name (empty str)
+        # within the default exchange situation the routing_key is specified by the queue name
+        # in order to tell the msg which queue it should go
+        self.channel.basic_publish(exchange='',
+                                   routing_key='hello',
+                                   body='Hello World!')
+        print(" [x] Sent 'Hello World!'")
+
+        # disconnect from rabbit
+        self.connection.close()
+
+
+if __name__ == '__main__':
+    s = Producer()
+    s.p1()
