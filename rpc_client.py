09562e5dac13b09431f5d7f6961a3ceb0eae4293
ubuntu16.04 <huangkai919@163.com>
2017-05-19 18:39:30 +0800

    add rpc

---
+++ rpc_client.py
@@ -0,0 +1,58 @@
+import pika
+import uuid
+import sys
+
+
+class RpcClient(object):
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+
+        self.channel = self.connection.channel()
+
+        # create a single callback queue per client
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.response = None
+        self.corr_id = ''
+
+        # subscribe the callback queue for receiving RPC response
+        self.channel.basic_consume(self.on_response,
+                                   no_ack=True,
+                                   queue=self.callback_queue)
+
+    def on_response(self, ch, method, props, body):
+        """
+        executed on every response
+        check the msg from callback queue if it's the response match the request
+        based on BasicProperties of correlation_id
+        """
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, n):
+        self.corr_id = str(uuid.uuid4())
+
+        # publish the request message, with two properties: 'reply_to' and 'correlation_id'.
+        # 'reply_to' specify queue used for response msg
+        self.channel.basic_publish(exchange='',
+                                   routing_key='rpc_queue',
+                                   properties=pika.BasicProperties(reply_to=self.callback_queue,
+                                                                   correlation_id=self.corr_id,),
+                                   body=str(n))
+
+        # wait until the proper response arrives
+        while self.response is None:
+            self.connection.process_data_events()
+        return self.response
+
+    def request(self):
+        n = int(sys.argv[1])
+        print(" [x] Requesting fib(30)")
+        response = self.call(n)
+        print(" [.] Got %r" % response)
+
+
+if __name__ == '__main__':
+    fc = RpcClient()
+    fc.request()
---
+++ rpc_server.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import pika
+
+
+class RpcServer(object):
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='rpc_queue')
+
+    @staticmethod
+    def fib(n):
+        if n == 0:
+            return 0
+        else:
+            res = []
+            a, b = 0, 1
+            while a < n:
+                res.append(a)
+                a, b = b, a+b
+            return res
+
+    def _on_request(self, ch, method, props, body):
+        num = int(body)
+        print(" [.] fib(%s)" % num)
+        response = self.fib(num)
+        ch.basic_publish(exchange='',
+                         routing_key=props.reply_to,
+                         properties=pika.BasicProperties(correlation_id=props.correlation_id),
+                         body=str(response))
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    def serve(self):
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(self._on_request, queue='rpc_queue')
+        print(" [x] Awaiting RPC requests")
+        self.channel.start_consuming()
+
+if __name__ == '__main__':
+    rs = RpcServer()
+    rs.serve()
+
